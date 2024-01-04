cf86eccb7fe476b3b2bbdf17dd01575626cfef6a
Meroujan Antonyan (AM-CERT) <meroujan.antonyan@am-cert.am>
2024-01-04 14:28:30 +0400

    log short source files

--- internal/log.go
+++ internal/log.go
@@ -3,10 +3,22 @@ package internal
 import (
 	"github.com/rs/zerolog"
 	"os"
+	"strconv"
 	"time"
 )
 
 func InitLogger() zerolog.Logger {
+	zerolog.CallerMarshalFunc = func(file string, line int) string {
+		short := file
+		for i := len(file) - 1; i > 0; i-- {
+			if file[i] == '/' {
+				short = file[i+1:]
+				break
+			}
+		}
+		file = short
+		return file + ":" + strconv.Itoa(line)
+	}
 	return zerolog.New(
 		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
 	).Level(zerolog.InfoLevel).With().Timestamp().Caller().Logger()
